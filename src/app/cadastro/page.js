26a3c5f09d9a81bc04072a55bf2f22cfdb15f33e
agathasoaresx <agatacsoares@gmail.com>
2023-09-14 21:36:26 -0300

    first commit

---
+++ src/app/cadastro/page.js
@@ -0,0 +1,19 @@
+const Cadastro = () => {
+  return (
+    <div class="flex">
+    <div id="column-1" class="flex w-1/3 bg-gradient-to-r flex items-center justify-center from-slate-50 to-blue-100 h-screen rounded-br-3xl">
+        <p class="text-lg">Já possui conta? <a href="#" class="text-yellow-300 font-bold">Faça o login</a></p>    
+    </div>
+
+    <div id="column-2" class="flex w-2/3">
+        <div id="cadastro-card" class="border ">
+        <div id="cadastro-title" class="border">
+        <h2></h2>
+        </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Cadastro
